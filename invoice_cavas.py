30bda13381d746ada7a6aadd70d78ae64b359765
Jose Hernandez <jhernandez.sld@e.email>
2021-05-31 14:37:27 -0500

    added email sender

--- invoice_cavas.py
+++ invoice_cavas.py
@@ -12,6 +12,31 @@ from pyinvoice.templates import SimpleInvoice
 from typing import Dict, List
 import json
 from pathlib import Path
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+
+def send_pdf_mail(pass_data: Dict[str, str], receipents, pdf_path: str):
+    msg = MIMEMultipart()
+    message = "Hi, this is my monthly invoice"
+    # setup the parameters of the message
+    msg["From"] = pass_data["user"]
+    msg["To"] = ", ".join(receipents)
+    msg["Subject"] = "Invoice"
+    # add in the message body
+    msg.attach(MIMEText(message, "plain"))
+    msg.attach(MIMEText(open(pdf_path).read()))
+    # create server
+    server = smtplib.SMTP("smtp.office365.com:587")
+    server.starttls()
+    # Login Credentials for sending the mail
+    server.login(pass_data["user"], pass_data["pass"])
+
+    # send the message via the server.
+    server.sendmail(msg["From"], receipents, msg.as_string())
+    server.quit()
+    print("successfully sent email to %s:" % (msg["To"]))
 
 
 def create_invoice(
@@ -21,9 +46,10 @@ def create_invoice(
     items: List[Item] = [],
     invoice_number: int = 0,
     invoice_date: datetime = datetime.now(),
-):
+) -> str:
     invoice_date_str = invoice_date.strftime("%y%m")
-    doc = SimpleInvoice(f"{str(invoice_dir)}/invoice{invoice_date_str}.pdf")
+    pdf_path = f"{str(invoice_dir)}/invoice{invoice_date_str}.pdf"
+    doc = SimpleInvoice(pdf_path)
     doc.invoice_info = InvoiceInfo(invoice_number, invoice_date)
     doc.service_provider_info = ServiceProviderInfo(**provider_info)
     doc.client_info = ClientInfo(**client_info)
@@ -31,6 +57,7 @@ def create_invoice(
         doc.add_item(_item)
     doc.set_bottom_tip("Thank you for your business.")
     doc.finish()
+    return pdf_path
 
 
 def month_diff(start_date: datetime, end_date: datetime):
@@ -53,7 +80,7 @@ def create_spectrum_invoice(
     billed_month = invoice_date - relativedelta(months=1)
     invoice_number = month_diff(start_date, invoice_date)
     items = [Item(billed_month.strftime("%B"), "", 1, month_salary)]
-    create_invoice(
+    return create_invoice(
         Path(invoice_dir),
         provider_info=provider_info,
         client_info=client_info,
@@ -64,4 +91,8 @@ def create_spectrum_invoice(
 
 
 if __name__ == "__main__":
-    create_spectrum_invoice(invoice_dir="/Users/jhernandez/Documents/Accounting/SpectrumInvoices/")
+    pass_data = {}
+    with open("pass.json") as f:
+        pass_data = json.load(f)
+    pdf_path = create_spectrum_invoice()
+    send_pdf_mail(pass_data=pass_data, receipents=["test@email.com"], pdf_path=pdf_path)
